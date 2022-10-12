0fb08ef3b9f1ab91ba8ba59fbcfbb3ca23e62bf6
sjesun <71446111+sjesun@users.noreply.github.com>
2022-10-12 19:18:51 -0400

    Update pysql.py
    pysql.py now creates a database "webapp", creates tables as per create_db.sql and populates the University table with data from university.csv.

--- pysql.py
+++ pysql.py
@@ -3,20 +3,37 @@ import mysql.connector
 db = mysql.connector.connect(
     host="localhost",
     user="root",
-    passwd="root"
+    passwd="root",
+    allow_local_infile=True
     )
     
 cur = db.cursor(dictionary=True)
 
-# creating tables
-with open('create_db.sql', 'r') as sqlfile:
-    query_iter = cur.execute(sqlfile.read(), multi=True)
-    for q in query_iter:
-        print("Running: ", q)
-        print(f"{q.rowcount} rows changed")
-    db.commit()
+filelist = ['create_db.sql', 'load_university.sql']
 
-# Verifying the tables have been created
+# executes commands in sql files
+for file in filelist:
+    with open(file, 'r') as sqlfile:
+        query_iter = cur.execute(sqlfile.read(), multi=True)
+        for q in query_iter:
+            print("Running: ", q)
+            print(f"{q.rowcount} rows changed")
+        db.commit()
+
+# shows databases
+cur.execute("SHOW DATABASES")
+for item in cur:
+    print(item)
+
+# shows tables
 cur.execute("SHOW TABLES")
 for item in cur:
     print(item)
+
+''' For verifying that the University table was populated
+
+cur.execute("SELECT * FROM University")
+for item in cur:
+    print(item)
+
+'''

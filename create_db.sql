8b8f591ecb855e05f5a7e251ec14866325268977
sjesun <71446111+sjesun@users.noreply.github.com>
2022-10-11 21:38:07 -0400

    Update create_db.sql

--- create_db.sql
+++ create_db.sql
@@ -1,5 +1,4 @@
--- DROP DATABASE IF EXISTS webapp;
--- Uncomment above line to replace existing webapp database
+DROP DATABASE IF EXISTS webapp;
 
 CREATE DATABASE webapp;
 
@@ -7,53 +6,56 @@ USE webapp;
 
 CREATE TABLE University (
 	collegeID INT AUTO_INCREMENT, 
-    name VARCHAR(255) NOT NULL, 
-    city VARCHAR(50) NOT NULL, 
-    province VARCHAR(50) NOT NULL, 
-    country VARCHAR(50) NOT NULL, 
-    logoURL TEXT, 
-    rating FLOAT,
+	name VARCHAR(255) NOT NULL, 
+	city VARCHAR(50) NOT NULL, 
+	province VARCHAR(50) NOT NULL, 
+	country VARCHAR(50) NOT NULL, 
+	logoURL TEXT, 
+	rating FLOAT UNSIGNED,
+	CHECK (rating<=10),
 	PRIMARY KEY (collegeID)
 );
 
 CREATE TABLE Program (
 	programID INT AUTO_INCREMENT, 
-    collegeID INT NOT NULL, 
-    uni_name VARCHAR(255) NOT NULL, 
-    prog_name VARCHAR(255) NOT NULL,
-    PRIMARY KEY (programID) 
+	collegeID INT NOT NULL, 
+	uni_name VARCHAR(255) NOT NULL, 
+	prog_name VARCHAR(255) NOT NULL,
+	PRIMARY KEY (programID) 
 --    FOREIGN KEY (collegeID) REFERENCES University(collegeID), 
 --    FOREIGN KEY (uni_name) REFERENCES University(name)
 );
 
 CREATE TABLE Profile (
-    userID INT AUTO_INCREMENT, 
-    name VARCHAR(50) NOT NULL, 
-    email VARCHAR(50) NOT NULL UNIQUE, 
-    LinkedIn VARCHAR(255), 
-    joinedDate DATETIME DEFAULT CURRENT_TIMESTAMP, 
-    attended VARCHAR(255) NOT NULL, 
-    yearStarted INT NOT NULL, 
-    yearEnded INT, 
-    program VARCHAR(255), 
-    degreeType VARCHAR(50) NOT NULL, 
-    PRIMARY KEY (userID) 
+	userID INT AUTO_INCREMENT, 
+	name VARCHAR(50) NOT NULL, 
+	email VARCHAR(50) NOT NULL, 
+	LinkedIn VARCHAR(255), 
+	joinedDate DATETIME DEFAULT CURRENT_TIMESTAMP, 
+	attended VARCHAR(255) NOT NULL, 
+	yearStarted INT UNSIGNED NOT NULL, 
+	yearEnded INT UNSIGNED, 
+	program VARCHAR(255), 
+	degreeType VARCHAR(25) NOT NULL,
+--	CHECK (yearStarted <= YEAR(NOW())),
+	PRIMARY KEY (userID)
 --    FOREIGN KEY (attended) REFERENCES University(name), 
 --    FOREIGN KEY (program) REFERENCES Program(prog_name)
 );
 
 CREATE TABLE Review (
 	reviewID INT AUTO_INCREMENT, 
-    timeWritten DATETIME DEFAULT CURRENT_TIMESTAMP, 
-    writerID INT NOT NULL, 
-    writtenBy VARCHAR(50) NOT NULL, 
-    reviewBody TEXT, 
-    difficulty TINYINT NOT NULL, 
-    price TINYINT NOT NULL, 
-    academics TINYINT NOT NULL, 
-    studentLife TINYINT NOT NULL, 
-    recommend BOOL NOT NULL, 
-    PRIMARY KEY (reviewID) 
---	  FOREIGN KEY (writerID) REFERENCES Profile(userID) ON DELETE CASCADE, 
+	timeWritten DATETIME DEFAULT CURRENT_TIMESTAMP, 
+	writerID INT NOT NULL, 
+	writtenBy VARCHAR(50) NOT NULL, 
+	reviewBody TEXT, 
+	difficulty TINYINT UNSIGNED NOT NULL, 
+	price INT UNSIGNED NOT NULL,  -- change to ROI, or remove if needed
+	academics TINYINT UNSIGNED NOT NULL, 
+	studentLife TINYINT UNSIGNED NOT NULL, 
+	recommend enum('N','Y') NOT NULL,
+	CONSTRAINT CHK_Review CHECK (difficulty<=10 AND academics<=10 AND studentLife<=10),
+	PRIMARY KEY (reviewID) 
+--    FOREIGN KEY (writerID) REFERENCES Profile(userID) ON DELETE CASCADE, 
 --    FOREIGN KEY (writtenBy) REFERENCES Profile(name)
 );

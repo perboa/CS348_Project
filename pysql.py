a8c4a6d9c7be4aa625916145e4ac8b6628730596
sjesun <71446111+sjesun@users.noreply.github.com>
2022-10-12 22:19:01 -0400

    Add files via upload
    load_data.sql replaces load_university.sql. It populates both the University and Program tables.

--- pysql.py
+++ pysql.py
@@ -3,15 +3,14 @@ import mysql.connector
 db = mysql.connector.connect(
     host="localhost",
     user="root",
-    passwd="root",
+    passwd="jason",
     allow_local_infile=True
     )
     
 cur = db.cursor(dictionary=True)
 
-filelist = ['create_db.sql', 'load_university.sql']
+filelist = ['create_db.sql', 'load_data.sql']
 
-# executes commands in sql files
 for file in filelist:
     with open(file, 'r') as sqlfile:
         query_iter = cur.execute(sqlfile.read(), multi=True)
@@ -20,20 +19,14 @@ for file in filelist:
             print(f"{q.rowcount} rows changed")
         db.commit()
 
-# shows databases
 cur.execute("SHOW DATABASES")
 for item in cur:
     print(item)
 
-# shows tables
 cur.execute("SHOW TABLES")
 for item in cur:
     print(item)
 
-''' For verifying that the University table was populated
-
-cur.execute("SELECT * FROM University")
+cur.execute("SELECT * FROM Program")
 for item in cur:
     print(item)
-
-'''

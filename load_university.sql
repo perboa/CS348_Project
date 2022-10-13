3fb20706e4f4cd062fcbaa481593dd5b3d579ee9
sjesun <71446111+sjesun@users.noreply.github.com>
2022-10-12 22:19:59 -0400

    Delete load_university.sql

--- load_university.sql
+++
@@ -1,8 +0,0 @@
-SET GLOBAL local_infile=1;
-
-LOAD DATA LOCAL INFILE 'university.csv'
-INTO TABLE University
-FIELDS TERMINATED BY ','
-OPTIONALLY ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-(collegeID, uni_name, city, province, country, logoURL, rating);

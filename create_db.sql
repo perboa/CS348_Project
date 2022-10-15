82ac203d76fe07b716286bcca842dba4d90dacd2
sjesun <71446111+sjesun@users.noreply.github.com>
2022-10-15 19:41:55 -0400

    Update create_db.sql
    New "Attended" table added, creates redundancies with "User" which need to be cleaned up.

--- create_db.sql
+++ create_db.sql
@@ -20,15 +20,19 @@ CREATE TABLE Program (
 	programID INT AUTO_INCREMENT, 
 	collegeID INT NOT NULL, 
 	prog_name VARCHAR(255) NOT NULL,
+	degree_type enum('Undergraduate'. 'Graduate'), 
 	PRIMARY KEY (programID),
 	FOREIGN KEY (collegeID) REFERENCES University(collegeID)
 );
 
-CREATE TABLE Profile (
+CREATE TABLE User (
 	userID INT AUTO_INCREMENT, 
-	name VARCHAR(50) NOT NULL, 
-	email VARCHAR(50) NOT NULL, 
-	LinkedIn VARCHAR(255), 
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+	avatar TEXT,
+	email VARCHAR(50) NOT NULL,
+	password VARCHAR(50) NOT NULL,
+	linkedIn VARCHAR(255), 
 	joinedDate DATETIME DEFAULT CURRENT_TIMESTAMP,
 	collegeID INT NOT NULL,
 	uni_name VARCHAR(255) NOT NULL, 
@@ -37,11 +41,25 @@ CREATE TABLE Profile (
 	programID INT,
 	program VARCHAR(255),
 	degreeType VARCHAR(25) NOT NULL,
---	CHECK (yearStarted <= YEAR(NOW())),
+	CHECK (yearStarted >= 1940),
+	CHECK (yearEnded IS NULL OR yearStarted <= yearEnded), 
 	PRIMARY KEY (userID),
 	FOREIGN KEY (collegeID) REFERENCES University(collegeID),
 	FOREIGN KEY (programID) REFERENCES Program(programID)
-);
+); -- remove redundancy
+
+CREATE TABLE Attended (
+	attID INT AUTO_INCREMENT,
+	userID INT NOT NULL,
+	collegeID INT NOT NULL,
+	programID INT,
+	yearStarted INT UNSIGNED,
+	yearEnded INT UNSIGNED,
+	PRIMARY KEY (attID),
+	FOREIGN KEY (userID) REFERENCES User(UserID) ON DELETE CASCADE,
+	FOREIGN KEY (collegeID) REFERENCES User(collegeID),
+	FOREIGN KEY (programID) REFERENCES User(programID)
+); -- remove redundancy
 
 CREATE TABLE Review (
 	reviewID INT AUTO_INCREMENT, 
@@ -58,7 +76,7 @@ CREATE TABLE Review (
 	recommend enum('N', 'Y') NOT NULL,
 	CONSTRAINT CHK_Review CHECK (difficulty<=10 AND academics<=10 AND studentLife<=10),
 	PRIMARY KEY (reviewID),
-	FOREIGN KEY (writerID) REFERENCES Profile(userID) ON DELETE CASCADE,
+	FOREIGN KEY (writerID) REFERENCES User(userID) ON DELETE CASCADE,
 	FOREIGN KEY (collegeID) REFERENCES University(collegeID),
 	FOREIGN KEY (programID) REFERENCES Program(programID)
 );

904d3cbe70f0f2b58588a8d4b6c3e8bee5b4e9c9
DanielZotkin <79817766+DanielZotkin@users.noreply.github.com>
2022-11-15 21:05:56 -0500

    Header stuff

---
+++ src/header.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import SearchBar from "./search.js";
+import ReactDOM from 'react-dom';
+import { AppShell, Navbar, Header, Aside, Footer, Text, Group, Button } from '@mantine/core';
+import "./index.css"
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  withRouter
+} from 'react-router-dom';
+const SiteHeader = () => (
+    <form action="/" method="get">
+        <Header height={{base: 70, md: 70 }} p="md">
+          <Group sx={{ height: '100%' }} px={20} position="apart">
+              <Text size="lg" weight="boldest">University Searcher</Text>
+              <Button variant="gradient" gradient={{ from: 'indigo', to: 'cyan' }} onclick={() => {
+                <Router>
+                  <Routes>
+                    <Route exact path='/'></Route>
+                  </Routes>
+                </Router>
+              }}>Home</Button>
+                
+              
+          </Group>
+          
+        </Header>
+    </form>
+);
+
+export default SiteHeader;

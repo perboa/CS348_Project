3bcc6d9291f5aa61d938cf1d4c9d79d9d11f9865
sjesun <71446111+sjesun@users.noreply.github.com>
2022-11-17 20:23:38 -0500

    Using Mantine for forms, so commented this out

--- api/signup.py
+++ api/signup.py
@@ -1,14 +1,14 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Email, Length, EqualTo
+# from flask_wtf import FlaskForm
+# from wtforms import StringField, PasswordField, SubmitField, BooleanField
+# from wtforms.validators import DataRequired, Email, Length, EqualTo
 
 
-class SignupForm(FlaskForm):
-    first_name = StringField('First Name', validators=[DataRequired()])
-    last_name = StringField('Last Name', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email(message='Invalid email.'),
-                                             Length(max=64, message='Email address too long.')])
-    password = PasswordField('Password', validators=[DataRequired(),
-                                                     Length(min=8, message='Must have at least 8 characters.')])
-    confirm = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Sign Up')
+# class SignupForm(FlaskForm):
+    # first_name = StringField('First Name', validators=[DataRequired()])
+    # last_name = StringField('Last Name', validators=[DataRequired()])
+    # email = StringField('Email', validators=[DataRequired(), Email(message='Invalid email.'),
+                                             # Length(max=64, message='Email address too long.')])
+    # password = PasswordField('Password', validators=[DataRequired(),
+                                                     # Length(min=8, message='Must have at least 8 characters.')])
+    # confirm = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    # submit = SubmitField('Sign Up')

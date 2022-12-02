aade7fc1910762f75e5af98d19cc1e19003ecfd5
sjesun <71446111+sjesun@users.noreply.github.com>
2022-12-01 21:43:53 -0500

    Add files via upload

---
+++ sql/feature 4.sql
@@ -0,0 +1,19 @@
+SELECT AVG(recommend)
+FROM review
+WHERE college_id = 1;
+
+SELECT AVG(difficulty)
+FROM review
+WHERE college_id = 1;
+
+SELECT AVG(employer_reputation)
+FROM review
+WHERE college_id = 1;
+
+SELECT AVG(student_life)
+FROM review
+WHERE college_id = 1;
+
+SELECT AVG(academics)
+FROM review
+WHERE college_id = 1;

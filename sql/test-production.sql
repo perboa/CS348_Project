3fd16efcac0f897bf380b9962d7e49babfe4325f
sjesun <71446111+sjesun@users.noreply.github.com>
2022-12-01 21:48:00 -0500

    Update test-production.sql

--- sql/test-production.sql
+++ sql/test-production.sql
@@ -6,7 +6,25 @@ VALUES("Test", "User", "testuser@gmail.com", "123",  CURRENT_TIMESTAMP);
 INSERT INTO review (time_written, user_id, college_id, body, difficulty, employer_reputation, academics, student_life, recommend) 
 VALUES (CURRENT_TIME(), 1 , 1, "This is my review.", 1, 2, 3, 5, True); 
 
-DELETE FROM review WHERE user_id = 1 AND college_id = 1;
+SELECT AVG(recommend)
+FROM review
+WHERE college_id = 1;
+
+SELECT AVG(difficulty)
+FROM review
+WHERE college_id = 1;
+
+SELECT AVG(employer_reputation)
+FROM review
+WHERE college_id = 1;
+
+SELECT AVG(student_life)
+FROM review
+WHERE college_id = 1;
+
+SELECT AVG(academics)
+FROM review
+WHERE college_id = 1;
 
 SELECT * FROM review WHERE college_id = 2 ORDER BY time_written DESC;
 
